ac9ceeb6865416db7f59c53ad0b915ba9b0cb5cb
izzylppz <https://github.com/Shmizzy>
2024-04-22 13:56:52 -0500

    initial commit

--- server.js
+++ server.js
@@ -31,8 +31,25 @@ app.get('/links', (req, res) => {
 app.get('/menu', (req, res) => {
     res.render('menu.ejs', {data: data.companyData})
 });
+app.get('/menu/:itemInput', (req, res) => {
+    const itemInput = req.params.itemInput;
+    const menu = data.companyData.menu;
+
+    
+    for(let i = 0; i < menu.length; i++){
+        if(itemInput === menu[i].name){
+            res.render('specific-item.ejs', {
+                data: data.companyData,
+                item: menu[i]
+            })
+        }
+    }
+    
+    
+});
 
 
 app.listen(3000, () => {
     console.log('We are listening to port 3000');
 });
+
